337b64a550b1b1a9ad59b81339b2f386a4991e96
bakahao <shchng43@gmail.com>
2024-06-06 00:24:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/objectowl/LoginPageActivity.java
@@ -0,0 +1,75 @@
+package com.example.objectowl;
+
+import android.graphics.Color;
+import android.graphics.Outline;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.LinkMovementMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.view.ViewOutlineProvider;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+
+public class LoginPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        // Set up "Register here" clickable and underlined
+        TextView registerText = findViewById(R.id.register_text);
+        SpannableString spannableString = new SpannableString(registerText.getText());
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                // Handle click event, e.g., open registration activity
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(true);
+                ds.setColor(ContextCompat.getColor(getApplicationContext(), R.color.orange));
+            }
+        };
+
+        spannableString.setSpan(clickableSpan, 16, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        registerText.setText(spannableString);
+        registerText.setMovementMethod(LinkMovementMethod.getInstance());
+        registerText.setHighlightColor(Color.TRANSPARENT);
+
+        EditText passwordEditText = findViewById(R.id.password);
+        ToggleButton toggleButton = findViewById(R.id.toggle_password);
+
+        toggleButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                // Show password
+                passwordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            } else {
+                // Hide password
+                passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            }
+        });
+
+        ImageView logo = findViewById(R.id.logo);
+        logo.setClipToOutline(true);
+        logo.setOutlineProvider(new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                outline.setRoundRect(0, 0, view.getWidth(), view.getHeight(), 16 * getResources().getDisplayMetrics().density);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/objectowl/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.objectowl;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DISPLAY_LENGTH = 2000; // Duration of the splash screen in milliseconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // After the specified time, start the main activity
+                Intent mainIntent = new Intent(SplashActivity.this, LoginPageActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
+
