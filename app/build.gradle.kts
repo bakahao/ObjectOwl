592ea601a0c18317284398f219337abd5e0630a8
shenhao043 <82090803+shenhao043@users.noreply.github.com>
2024-10-01 00:26:54 +0800

    Latest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        mlModelBinding = true
+    }
 }
 
 dependencies {
@@ -40,6 +43,8 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.database)
+    implementation(libs.tensorflow.lite.support)
+    implementation(libs.tensorflow.lite.metadata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/objectowl/HomePageActivity.java
+++ app/src/main/java/com/example/objectowl/HomePageActivity.java
@@ -1,20 +1,42 @@
 package com.example.objectowl;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.service.controls.templates.ThumbnailTemplate;
 import android.view.MenuItem;
 import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.example.objectowl.ml.HouseHoldModelV1;
 import com.google.android.material.navigation.NavigationView;
+import android.Manifest;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class HomePageActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle toggle;
     private NavigationView navigationView;
+    TextView result, confidence;
+    ImageView imageView;
+    int imageSize = 224;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,16 +108,23 @@ public class HomePageActivity extends AppCompatActivity {
     // Methods to handle clicks for each menu item
     private void handleHomeClick() {
         // TODO: Add your action for the Home button here
-
     }
 
     private void handleRecognizeClick() {
-        // TODO: Add your action for the Recognize Object button here
+        // Check if the camera permission is granted
+        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            //Start Camera intent if permission is granted
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, 1);
+        } else {
+            //Request camera permission if we don't have it.
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+        }
     }
 
     private void handleGuideClick() {
         // TODO: Add your action for the User Guide button here
-        // Navigate to LoginPageActivity when logout is clicked
+        // Navigate to User Guide Page
         Intent intent = new Intent(HomePageActivity.this, UserGuidePageActivity.class);
         startActivity(intent);
         finish(); // Close the HomePageActivity to prevent returning with the back button
@@ -110,5 +139,81 @@ public class HomePageActivity extends AppCompatActivity {
         Intent intent = new Intent(HomePageActivity.this, LoginPageActivity.class);
         startActivity(intent);
         finish(); // Close the HomePageActivity to prevent returning with the back button
+    }
+
+    //Household model
+    public void classifyImage(Bitmap image){
+        try {
+            HouseHoldModelV1 model = HouseHoldModelV1.newInstance(getApplicationContext());
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            int [] intValues = new int[imageSize * imageSize];
+            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+            int pixel = 0;
+            for(int i = 0; i < imageSize; i++){
+                for(int j = 0; j< imageSize; j++){
+                    int val = intValues[pixel++];
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255.f));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255.f));
+                    byteBuffer.putFloat((val & 0xFF) * (1.f / 255.f));
+                }
+            }
+
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            HouseHoldModelV1.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            //find highest confidence value of the scanned image
+            float[] confidences = outputFeature0.getFloatArray();
+            int maxPos = 0;
+            float maxConfidence = 0;
+            for(int i =0; i < confidences.length; i++){
+                if(confidences[i] > maxConfidence){
+                    maxConfidence = confidences[i];
+                    maxPos = i;
+                }
+            }
+            String[] classes = {"Table", "Chair", "Sofa", "Bed", "Television",
+                                "Lamp", "Fan", "Cup", "Plate", "Spoon",
+                                "Fork", "Knife", "Toothbrush", "Towel", "Fridge",
+                                "Remote Control", "Clock", "Mirror"};
+
+            result.setText(classes[maxPos]);
+
+            //show the confidence for all classes
+            String s = "";
+            for(int i = 0; i < classes.length; i++){
+                s += String.format("%s: %.1f%%\n", classes[i], confidences[i] * 100);
+            }
+            confidence.setText(s);
+
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            //get the image taken by user
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+
+            //crop the image to fix tensorflow lite dimension
+            int dimension = Math.min(image.getWidth(), image.getHeight());
+            image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
+            imageView.setImageBitmap(image);
+
+            image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+            classifyImage(image);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
